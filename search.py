e239718eed72ea0ea8a30a576c4dddac01698fc2
Arvee1 <160126772+Arvee1@users.noreply.github.com>
2024-05-04 16:40:02 +1000

    Update search.py

--- search.py
+++ search.py
@@ -10,5 +10,25 @@ tool = Tool(
     func=search.run,
 )
 
-result = tool.run("Obama's first name?")
-st.write(result)
+st.title("👨‍💻 Wazzup!!!! I am Arvee's Personal Assistant?")
+prompt = st.text_area("Please enter what you want to know.")
+
+if st.button("Submit to AI", type="primary"):
+    result = tool.run(prompt)
+    result_ai = ""
+         for event in replicate.stream(
+             "meta/meta-llama-3-70b-instruct",
+             input={
+                 "top_k": 50,
+                 "top_p": 0.9,
+                 "prompt": "Prompt: " + result
+                 "max_tokens": 512,
+                 "min_tokens": 0,
+                 "temperature": 0.6,
+                 "prompt_template": "<|begin_of_text|><|start_header_id|>system<|end_header_id|>\n\nYou are a helpful assistant<|eot_id|><|start_header_id|>user<|end_header_id|>\n\n{prompt}<|eot_id|><|start_header_id|>assistant<|end_header_id|>\n\n",
+                 "presence_penalty": 1.15,
+                 "frequency_penalty": 0.2
+             },
+         ):
+             result_ai = result_ai + (str(event))
+         st.write(result_ai)
