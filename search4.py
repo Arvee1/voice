93c48db16b2ab0b8d153185c832dea9a236e7f8c
Arvee1 <160126772+Arvee1@users.noreply.github.com>
2024-05-20 19:25:58 +1000

    Update search4.py

--- search4.py
+++ search4.py
@@ -53,11 +53,12 @@ def get_text():
 user_input = get_text()
 
 if user_input:
-    result = tool.run(user_input)
+    # result = tool.run(user_input)
     # chain_prompt = user_input + " " + result
     # st.write(chain_prompt)
-    output = chain.run(input=user_input + ", " + result)
-
+    # output = chain.run(input=user_input + ", " + result)
+    output = chain.run(input=user_input)
+    
     st.session_state.past.append(user_input)
     st.session_state.generated.append(output)
     # st.write(st.session_state)
