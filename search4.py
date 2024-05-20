3e3618d89e9915ce02aa9b4c3452b0f6f46de43d
Arvee1 <160126772+Arvee1@users.noreply.github.com>
2024-05-20 23:02:58 +1000

    Update search4.py

--- search4.py
+++ search4.py
@@ -150,7 +150,7 @@ if user_input:
           query_texts=[user_input],
           # include=["documents", "embeddings"],
           include=["documents"],
-          n_results=5,
+          n_results=10,
     )
     augment_query = str(query_results["documents"])
     chain_input = user_input + ", " + augment_query
